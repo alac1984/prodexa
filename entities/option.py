5581e766eb8cdb133be52f09d6396c767b3cdc7a
André Carvalho <alac1984@gmail.com>
2023-04-02 12:47:19 -0300

    :sparkles: Create option entity

---
+++ entities/option.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from .image import Image
+
+
+@dataclass
+class Option:
+    letter: str
+    text: str
+    image: Image | None = None
+    _complete: bool = False
+
+    @property
+    def is_complete(self):
+        return self._complete
