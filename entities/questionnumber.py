18a923886bb0a0707117fa53dfa14f46c4ea817f
André Carvalho <alac1984@gmail.com>
2023-04-02 13:53:18 -0300

    :boom: Change QuestionNumber Entity

--- entities/questionnumber.py
+++ entities/questionnumber.py
@@ -6,6 +6,11 @@ class QuestionNumber:
     raw: str
     number: int
     decorator: str
+    _complete: bool
 
     def normalized(self):
         return f"{self.number}. "
+
+    @property
+    def is_complete(self):
+        return self._complete
