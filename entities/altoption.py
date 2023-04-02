bbc8352732b6213e7f4456f2e7bc4bb5c5a6fbb5
André Carvalho <alac1984@gmail.com>
2023-04-02 13:22:52 -0300

    :sparkles: Change AltOptions Entity

--- entities/altoption.py
+++ entities/altoption.py
@@ -1,5 +1,15 @@
-from .option import Option
+from dataclasses import dataclass
+
+from .image import Image
 
 
-class AltOption(Option):
-    ...
+@dataclass
+class AltOption:
+    letter: str
+    text: str
+    image: Image | None = None
+    _complete: bool = False
+
+    @property
+    def is_complete(self):
+        return self._complete
